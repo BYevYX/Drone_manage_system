b82129a3516656749cca48c208637241dae1e9de
Аскеров Аскер Расимович <asraaskerov@edu.hse.ru>
2025-11-10 10:07:18 +0300

    fix bugs next

--- src/app/dashboards/ActiveMenuContext.tsx
+++
@@ -1,9 +0,0 @@
-// ActiveMenuContext.tsx
-import { createContext, useContext } from 'react';
-
-export const ActiveMenuContext = createContext({
-  activeMenu: 'client/dashboard',
-  setActiveMenu: (val: string) => {},
-});
-
-export const useActiveMenu = () => useContext(ActiveMenuContext);
